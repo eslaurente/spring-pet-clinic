650cea73980af5517f5e6b7994ea50ca48578acc
Emerald Laurente <e.s.laurente@gmail.com>
2020-05-24 17:31:17 -0700

    Spring initializr app init

---
+++ src/test/java/laurente/reference/petclicnicapp/PetClicnicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package laurente.reference.petclicnicapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClicnicAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
