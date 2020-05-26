e76aa85c5c48fdcbd73b60945e1363e179da4be1
Emerald Laurente <e.s.laurente@gmail.com>
2020-05-26 00:38:08 -0700

    Refactored services with new CrudService interface

--- pet-clinic-data/src/main/java/laurente/reference/petclinicapp/services/PetService.java
+++ pet-clinic-data/src/main/java/laurente/reference/petclinicapp/services/PetService.java
@@ -2,13 +2,5 @@ package laurente.reference.petclinicapp.services;
 
 import laurente.reference.petclinicapp.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
--- pet-clinic-data/src/main/java/laurente/reference/petclinicapp/services/VetService.java
+++ pet-clinic-data/src/main/java/laurente/reference/petclinicapp/services/VetService.java
@@ -2,15 +2,5 @@ package laurente.reference.petclinicapp.services;
 
 import laurente.reference.petclinicapp.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findByLastName(String lastName);
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
