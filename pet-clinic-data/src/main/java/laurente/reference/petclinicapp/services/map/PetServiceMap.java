edcaa3c7e7652b2b93644b747f2e77d8faa768d0
Emerald Laurente <e.s.laurente@gmail.com>
2020-05-26 09:51:13 -0700

    Implemented map classes

---
+++ pet-clinic-data/src/main/java/laurente/reference/petclinicapp/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package laurente.reference.petclinicapp.services.map;
+
+import laurente.reference.petclinicapp.model.Pet;
+import laurente.reference.petclinicapp.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+}
