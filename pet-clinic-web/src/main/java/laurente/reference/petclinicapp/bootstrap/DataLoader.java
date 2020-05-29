c5496143f925fe8db987cb2350319a98a4f59120
Emerald Laurente <e.s.laurente@gmail.com>
2020-05-29 15:01:13 -0700

    Added PetType data init bootstrap code

--- pet-clinic-web/src/main/java/laurente/reference/petclinicapp/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/laurente/reference/petclinicapp/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package laurente.reference.petclinicapp.bootstrap;
 
 import laurente.reference.petclinicapp.model.Owner;
+import laurente.reference.petclinicapp.model.PetType;
 import laurente.reference.petclinicapp.model.Vet;
 import laurente.reference.petclinicapp.services.OwnerService;
+import laurente.reference.petclinicapp.services.PetTypeService;
 import laurente.reference.petclinicapp.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,30 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) {
+        // Add pet type data
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatType = petTypeService.save(cat);
+
+        System.out.println("Saved dog type: " + dog);
+        System.out.println("Saved cat type: " + cat);
+
+        // Add owners data
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
@@ -33,6 +51,8 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner2);
 
         System.out.println("Successfully loaded owners ...");
+
+        // Add vets data
 
         Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
