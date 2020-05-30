344a6885eca4f31dfbcbe2674eadc5bae512f6b8
Emerald Laurente <e.s.laurente@gmail.com>
2020-05-29 23:12:34 -0700

    Implemented SpecialtyService interface and impl

---
+++ pet-clinic-data/src/main/java/laurente/reference/petclinicapp/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package laurente.reference.petclinicapp.services;
+
+import laurente.reference.petclinicapp.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/laurente/reference/petclinicapp/services/map/SpecialtyMapService.java
@@ -0,0 +1,36 @@
+package laurente.reference.petclinicapp.services.map;
+
+import laurente.reference.petclinicapp.model.Specialty;
+import laurente.reference.petclinicapp.services.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
