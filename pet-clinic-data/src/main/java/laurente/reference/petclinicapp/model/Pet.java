e0af31851ffa065a43a750bcf52c8ee415fd45e4
eslaurente <e.s.laurente@gmail.com>
2020-06-11 23:34:49 -0700

    Added JPA relationships to Owner, Person, Pet, and PetType

--- pet-clinic-data/src/main/java/laurente/reference/petclinicapp/model/Pet.java
+++ pet-clinic-data/src/main/java/laurente/reference/petclinicapp/model/Pet.java
@@ -1,11 +1,35 @@
 package laurente.reference.petclinicapp.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
+
+    @Column(name = "name")
+    public String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public PetType getPetType() {
         return petType;
