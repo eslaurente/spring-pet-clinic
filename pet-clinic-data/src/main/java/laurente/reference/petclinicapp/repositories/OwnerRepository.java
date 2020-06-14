69ac27319728e44cbec7797f9d023733cf5c4c74
Emerald Laurente <e.s.laurente@gmail.com>
2020-06-13 21:47:19 -0700

    Added JPA owner service

--- pet-clinic-data/src/main/java/laurente/reference/petclinicapp/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/laurente/reference/petclinicapp/repositories/OwnerRepository.java
@@ -1,6 +1,9 @@
 package laurente.reference.petclinicapp.repositories;
 
+import laurente.reference.petclinicapp.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
-public interface OwnerRepository extends CrudRepository<OwnerRepository, Long> {
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
 }
---
+++ pet-clinic-data/src/main/java/laurente/reference/petclinicapp/services/datajpa/OwnerJpaService.java
@@ -0,0 +1,63 @@
+package laurente.reference.petclinicapp.services.datajpa;
+
+import laurente.reference.petclinicapp.model.Owner;
+import laurente.reference.petclinicapp.repositories.OwnerRepository;
+import laurente.reference.petclinicapp.repositories.PetRepository;
+import laurente.reference.petclinicapp.repositories.PetTypeRepository;
+import laurente.reference.petclinicapp.services.OwnerService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.Spliterator;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+@Profile("springdatajpa")
+public class OwnerJpaService implements OwnerService {
+
+    private final OwnerRepository ownerRepository;
+    private final PetRepository petRepository;
+    private final PetTypeRepository petTypeRepository;
+
+    public OwnerJpaService(OwnerRepository ownerRepository, PetRepository petRepository,
+                           PetTypeRepository petTypeRepository) {
+        this.ownerRepository = ownerRepository;
+        this.petRepository = petRepository;
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Iterable<Owner> all = ownerRepository.findAll();
+        return StreamSupport.stream(all.spliterator(), false)
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return ownerRepository.findById(id)
+                .orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return ownerRepository.save(owner);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        ownerRepository.delete(owner);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        ownerRepository.deleteById(id);
+    }
+}
